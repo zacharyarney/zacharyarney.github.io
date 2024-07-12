9c76ca75c8e274ffaba92416ca5030236206e1aa
Zachary Arney <zachary.arney@gmail.com>
2024-07-11 21:39:26 -0700

    changes tabs based on url anchors for browser nav

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,23 +9,24 @@ class TabLink {
     );
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.item);
-    // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener('click', event => {
-      this.select(event);
-    });
+    // change tabs based on url anchor for browser navigation
+    window.addEventListener('hashchange', () => this.select());
   }
 
   select() {
-    // Get all the elements with the tabs-link class
-    let links = document.querySelectorAll('.tabs-link');
-    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all the links
-    Array.from(links).forEach(element => {
-      element.classList.remove('tabs-link-selected');
-    });
-    // Add a class named "tabs-link-selected" to this link
-    this.element.classList.add('tabs-link-selected');
-    // Call the select method on the item associated with this link
-    this.tabItem.select();
+    const anchor = location.hash ? location.hash.slice(1) : 'code';
+    if (this.item.classList.contains(anchor)) {
+      // Get all the elements with the tabs-link class
+      let links = document.querySelectorAll('.tabs-link');
+      // Using a loop or the forEach method remove the 'tabs-link-selected' class from all the links
+      Array.from(links).forEach(element => {
+        element.classList.remove('tabs-link-selected');
+      });
+      // Add a class named "tabs-link-selected" to this link
+      this.element.classList.add('tabs-link-selected');
+      // Call the select method on the item associated with this link
+      this.tabItem.select();
+    }
   }
 }
 
@@ -51,4 +52,4 @@ let links = document.querySelectorAll('.tabs-link');
 // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item.
 links = Array.from(links).map(link => new TabLink(link));
 // Once you have created an array of TabLink instances. call select() on the first item in the array
-links[0].select();
+links.forEach(link => link.select());
